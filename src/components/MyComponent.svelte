6155e6aa14fec870a0d7edb94358f2432e6b0065
mac_pro_2013 <mac_pro_2013@mac-pro-2013s-Mac-Pro.local>
2025-03-27 17:41:54 +0200

    Saving my local changes

---
+++ src/components/MyComponent.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let bobo = "Astro";
+    </script>
+
+    <p class="greeting">Hello, {bobo}!</p>
+
+    <style>
+        .greeting {
+        color: rgb(81, 81, 81);
+        font-size: 3em;
+        margin-left: 2rem;
+        }
+        </style>
